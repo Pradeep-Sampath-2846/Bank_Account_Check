cd8becb01739f9cc7ac8da567066f9dda033be50
pradeep sampath <109191549+Pradeep-Sampath-2846@users.noreply.github.com>
2023-05-21 12:11:06 +0530

    initial commit

---
+++ src/main/java/ps/account/checkmyaccount/CheckmyaccountApplication.java
@@ -0,0 +1,13 @@
+package ps.account.checkmyaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckmyaccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckmyaccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ps/account/checkmyaccount/CheckmyaccountApplicationTests.java
@@ -0,0 +1,13 @@
+package ps.account.checkmyaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckmyaccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
